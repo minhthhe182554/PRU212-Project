d6021e53548c61045b3e51415fc9df480eceb317
Hung Minh <minhthhe182554@fpt.edu.vn>
2025-06-12 22:19:51 +0700

    Add more scene names

--- Assets/Scripts/UI Control/SceneNames.cs
+++ Assets/Scripts/UI Control/SceneNames.cs
@@ -2,4 +2,5 @@ public static class SceneNames
 {
     public const string SETTING_SCENE = "SettingScene";
     public const string MAIN_MENU = "MainMenu";
+    public const string SELECT_PLAYER = "SelectPlayer";
 }
